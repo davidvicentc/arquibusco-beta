0889402bec083d43fa1e8d9356e38afe11fc46f5
David Vicent <djassiell@gmail.com>
2019-07-06 13:59:22 -0400

    Updates

---
+++ precache-manifest.8ad713a75ea4d2a330d9573cd60b0276.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "513fd4abe90eb713030d7237e09cb308",
+    "url": "/arquibusco-beta/index.html"
+  },
+  {
+    "revision": "b1f1e574db6396bdd9f7",
+    "url": "/arquibusco-beta/static/css/2.00129a64.chunk.css"
+  },
+  {
+    "revision": "015ec9996c79a3e85f30",
+    "url": "/arquibusco-beta/static/css/main.3aafaf09.chunk.css"
+  },
+  {
+    "revision": "b1f1e574db6396bdd9f7",
+    "url": "/arquibusco-beta/static/js/2.feb96f5e.chunk.js"
+  },
+  {
+    "revision": "015ec9996c79a3e85f30",
+    "url": "/arquibusco-beta/static/js/main.61a72de4.chunk.js"
+  },
+  {
+    "revision": "37a47dba3f52427b8843",
+    "url": "/arquibusco-beta/static/js/runtime~main.cdd7678f.js"
+  },
+  {
+    "revision": "d8eba9d5b17d2d9bdd76a186365f889e",
+    "url": "/arquibusco-beta/static/media/andy-kelly-416577-unsplash.d8eba9d5.jpg"
+  },
+  {
+    "revision": "e75a1199a3c79662bd17fc567f437fab",
+    "url": "/arquibusco-beta/static/media/arquitectura.e75a1199.jpg"
+  },
+  {
+    "revision": "f5fc57f111e0d4d3685458f1b5b72d8a",
+    "url": "/arquibusco-beta/static/media/artesania.f5fc57f1.jpg"
+  },
+  {
+    "revision": "dbef82894fcd99dffc932d485ae2a87f",
+    "url": "/arquibusco-beta/static/media/banner-photo3.dbef8289.png"
+  },
+  {
+    "revision": "35ea9d72a7818c5ad5c5524e11b8a213",
+    "url": "/arquibusco-beta/static/media/closet.35ea9d72.jpg"
+  },
+  {
+    "revision": "07620ae55b5f25d435c7c8cedd4b5328",
+    "url": "/arquibusco-beta/static/media/fotografia.07620ae5.jpg"
+  },
+  {
+    "revision": "b54553aa829e62d07981af957baf024b",
+    "url": "/arquibusco-beta/static/media/iluminacion.b54553aa.jpg"
+  },
+  {
+    "revision": "eaa3fac3cfbd7f66c7aadb145f1aeaab",
+    "url": "/arquibusco-beta/static/media/interiorismo.eaa3fac3.jpg"
+  },
+  {
+    "revision": "7504211f865b6401797ec9d490daf76e",
+    "url": "/arquibusco-beta/static/media/mobiliario.7504211f.jpg"
+  },
+  {
+    "revision": "51cbc6edde34ca5746af556be0a4d90f",
+    "url": "/arquibusco-beta/static/media/paisajismo.51cbc6ed.jpg"
+  },
+  {
+    "revision": "9f2abfaf5676bee7f1f981df4fdba37f",
+    "url": "/arquibusco-beta/static/media/publishwork2.9f2abfaf.png"
+  },
+  {
+    "revision": "928c5fc4f217c573d813ec6cbd336bf6",
+    "url": "/arquibusco-beta/static/media/renders.928c5fc4.jpg"
+  },
+  {
+    "revision": "ac0fce678467941d89dd85e3ae727ed4",
+    "url": "/arquibusco-beta/static/media/ricardo-gomez-angel-205726-unsplash.ac0fce67.jpg"
+  },
+  {
+    "revision": "75e49711615e169be430bdeb5e33f473",
+    "url": "/arquibusco-beta/static/media/selectdesigner2.75e49711.png"
+  }
+]);
